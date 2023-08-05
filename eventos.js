eefda421b66edbfc8d18fb3d2b1cdebfa35d7d52
deboragonzalezojeda <deboragonzalezojeda@gmail.com>
2023-08-05 16:57:46 -0300

    Correción del error de propagación
    Retire el evento "onclick" del código HTML e implementé la alert en el archivo eventos.js.

--- eventos.js
+++ eventos.js
@@ -4,3 +4,10 @@ console.log(div);
 div.addEventListener('click', function() {
     alert("Hola! Soy el div.");
 });
+
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    alert("Hola!");
+})
